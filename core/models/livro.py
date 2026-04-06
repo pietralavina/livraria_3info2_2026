be8c57fff37c54a6f121fc98a71acb96696f9475
pietralavina <pietragiovana2008@gmail.com>
2026-04-06 16:08:42 -0300

    feat: criação de múltiplos serializadores para Livro

--- core/models/livro.py
+++ core/models/livro.py
@@ -8,7 +8,7 @@ from .editora import Editora
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
-    quantidade = models.IntegerField(default=0,  null=True, blank=True)
+    quantidade = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     categoria = models.ForeignKey(
         Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True
@@ -17,4 +17,4 @@ class Livro(models.Model):
     autores = models.ManyToManyField(Autor, related_name='livros', blank=True)
 
     def __str__(self):
-        return f'({self.id}) {self.titulo} ({self.quantidade})'
+        return f'({self.id}) {self.titulo} ({self.quantidade})'  # noqa: W292
