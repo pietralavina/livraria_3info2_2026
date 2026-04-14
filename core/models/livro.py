71342451ba9b7fa9ef3d47411500bfdcf9b0b0eb
pietralavina <pietragiovana2008@gmail.com>
2026-04-14 15:00:13 -0300

    feat: add cidade and email fields to Editora model; update Livro model's preco field
    - Added 'cidade' (CharField) and 'email' (EmailField) to the Editora model.
    - Altered 'preco' field in Livro model to allow null and blank values.
    - Created migration files for the changes in the database schema.
    - Updated Editora string representation to include ID.

--- core/models/livro.py
+++ core/models/livro.py
@@ -11,7 +11,7 @@ class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0, null=True, blank=True)
-    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
     categoria = models.ForeignKey(
         Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True
     )
